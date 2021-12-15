9cf7c67e9f9920d8d5b3a211734023abd3b466e8
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-15 17:02:38 +0530

    without power on

--- src/com/Package/Main.java
+++ src/com/Package/Main.java
@@ -3,8 +3,12 @@ package com.Package;
 public class Main {
 
     public static void main(String[] args) {
-	    DeskPhone myPhone = new DeskPhone("7977424564");
-        myPhone.powerOn();
+//	    DeskPhone myPhone = new DeskPhone("7977424564");
+//        myPhone.powerOn();
+//        myPhone.callPhone("7977424564");
+//        myPhone.answer();
+
+        MobilePhone myPhone = new MobilePhone("7977424564");
         myPhone.callPhone("7977424564");
         myPhone.answer();
     }
